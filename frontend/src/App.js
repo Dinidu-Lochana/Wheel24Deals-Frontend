73762d2bb4f0a6346c6f30286ac160edff7d4344
Dinidu-Lochana <24.dinidulochana@gmail.com>
2025-02-17 11:17:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Wheels24Deals</h1>
+      <div>
+        <input type='text' placeholder='Vehicle name'></input>
+        <input type='number' placeholder='Price'></input>
+        <input type='text' placeholder='mileage'></input>
+        <button>Add Vehicle</button>
+        
+      </div>
+      
     </div>
   );
 }
